d749c06f533da53a43ca7fff0d9f3a110351badc
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-10-04 16:09:12 +0100

    Add Standard JS for linting and fixes any issues

--- tasks/gulp/build.js
+++ tasks/gulp/build.js
@@ -1,9 +1,8 @@
-const { task, src, dest} = require('gulp')
+const { task, src, dest } = require('gulp')
 const debug = require('gulp-debug')
 
 task('build:govuk-frontend', async () => {
-
   src(['node_modules/govuk-frontend/**'])
-    .pipe(debug({title: 'Copied'}))
-    .pipe( dest('package/govuk-frontend'))
+    .pipe(debug({ title: 'Copied' }))
+    .pipe(dest('package/govuk-frontend'))
 })
--- tasks/gulp/clean.js
+++ tasks/gulp/clean.js
@@ -6,11 +6,11 @@ const { green } = require('chalk')
 task('clean', async (done) => {
   const deletedPaths = await del(['package/*'])
   const colour = green.bold
-  const logTitle = (deletedPaths.length)? `Deleted files and directories:\n`: 'No files/folders to clean'
+  const logTitle = (deletedPaths.length) ? 'Deleted files and directories:\n' : 'No files/folders to clean'
   const logTitlePrefix = `${emoji.get('file_folder')}  - `
 
   if (deletedPaths.length) {
-    console.log(colour(logTitlePrefix + logTitle), `${deletedPaths.join('\n\s')}`)
+    console.log(colour(logTitlePrefix + logTitle), deletedPaths.join('\n  '))
   } else {
     console.log(colour(logTitlePrefix + logTitle))
   }
